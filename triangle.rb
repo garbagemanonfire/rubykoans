4fc6ef537b32ee779c0e980931cf1e550cdf7f15
garbagemanonfire <nilesvm@gmail.com>
2013-11-16 20:18:45 -0800

    Up to 211

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+    
+    raise TriangleError, "Sides must by numbers greater than zero" if (a <= 0) || (b <= 0) || (c <= 0)
+    raise TriangleError, "No two sides can add to be less than or equal to the other side" if (a+b <= c) || (a+c <= b) || (b+c <= a)
+
+    if ((a == b) && (c == b) && (a == c))
+        return :equilateral
+    elsif ((a == b) || (c == b) || (a == c))
+        return :isosceles   
+    else 
+        return :scalene    
+    end
+
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
+
 end
